dddb083a8411fc6e72091079a9b34aca649d72d0
Kundan Singh <kundan.singh979@gmail.com>
2022-12-25 17:45:23 +0400

    Updates

--- component---src-pages-about-js-9694597d54a10398c5e8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkHoomans=self.webpackChunkHoomans||[]).push([[682],{4633:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c}});var a=n(7294),o=n(6456),r=n(9357),s={color:"#232129",padding:"96px"},u={marginBottom:48},c=function(){return a.createElement(r.Z,{title:"About Us"})};t.default=function(){return a.createElement(o.Z,{pageTitle:"About Us"},a.createElement("div",{style:s},a.createElement("p",{style:u},"Hi 👋, we are Hoomans, and we create awesome things!.")))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-9694597d54a10398c5e8.js.map
